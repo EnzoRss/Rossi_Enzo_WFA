39fe0b2441515abe154df32a1469f1918597633f
EnzoRss <enzo.rossi@ynov.com>
2023-09-20 16:58:02 +0200

    ajout composant de base
    ajout de la base du projet , du cahier de dev , et des assets

---
+++ Space invader/Space invader/PLayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_invader
+{
+    public class PLayer
+    {
+        public int hp { get; set; }
+        public int point { get; set; }
+        public int lvl { get; set; }
+        public string name { get; set; }
+        
+        public PLayer (string name)
+        {
+            this.name = name;
+            hp = 100;
+            point = 0;
+            lvl = 1;
+        }
+    }
+}
